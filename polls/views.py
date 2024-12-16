33d640f8fcc2e719ec853253d70aee28bb8f62a9
Vincent Au <vincentwcau@gmail.com>
2024-12-16 11:31:40 -0500

    test: add testing code

--- polls/views.py
+++ polls/views.py
@@ -5,9 +5,9 @@ from django.template import loader
 from django.db.models import F
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
-
 
 # def index(request):
 #     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -41,12 +41,15 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        """ Return 5 recent questions"""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
