4bf3c3c2bf95588f7589007531d4c0056946ff13
Vincent Au <vincentwcau@gmail.com>
2024-12-16 14:03:38 -0500

    feat: update admin web

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,31 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date Information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_filter = ["question"]
+    list_display = ["question", "choice_text", "votes"]
+
+
+admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Choice, ChoiceAdmin)
